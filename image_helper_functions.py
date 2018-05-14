9891d727ad3d4e6cddea6e5813f0cbb03af43706
Matej Jularic <matej@morpheuszone.com>
2018-05-14 12:03:50 +0200

    Refactoring of draw_edges function

--- image_helper_functions.py
+++ image_helper_functions.py
@@ -10,29 +10,30 @@ def load_images(image):
 
 def draw_edges(graph, scale, offset, selected):
     for edge in graph.edges_iter():
-        if edge == selected:
-            pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2f', (
-                offset[0] + graph.node[edge[0]]["x"] * scale,
-                offset[1] + graph.node[edge[0]]["y"] * scale,
-                offset[0] + graph.node[edge[1]]["x"] * scale,
-                offset[1] + graph.node[edge[1]]["y"] * scale)),
-                                 ('c3B', (0,255,0,0,255,0)))
 
+        vertex_list = pyglet.graphics.vertex_list(2,
+                                                  ('v2f',
+                                                   (offset[0] + graph.node[edge[0]]["x"] * scale,
+                                                    offset[1] + graph.node[edge[0]]["y"] * scale,
+                                                    offset[0] + graph.node[edge[1]]["x"] * scale,
+                                                    offset[1] + graph.node[edge[1]]["y"] * scale)
+                                                   ),
+                                                  ('c3B/dynamic', (255, 255, 255, 255, 255, 255))
+                                                  )
+        if edge == selected:
+            vertex_list.colors[:6] = (0, 255, 0, 0, 255, 0)
+            vertex_list.draw(pyglet.gl.GL_LINES)
         else:
-            pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2f', (
-                offset[0] + graph.node[edge[0]]["x"] * scale,
-                offset[1] + graph.node[edge[0]]["y"] * scale,
-                offset[0] + graph.node[edge[1]]["x"] * scale,
-                offset[1] + graph.node[edge[1]]["y"] * scale)))
+            vertex_list.draw(pyglet.gl.GL_LINES)
 
 
 def draw_nodes(graph, scale, offset, selected, selected_sprite, node_sprite):
     for node in graph.nodes_iter():
         if node == selected:
             selected_sprite.position = (offset[0] + graph.node[node]["x"] * scale,
-                                             offset[1] + graph.node[node]["y"] * scale)
+                                        offset[1] + graph.node[node]["y"] * scale)
             selected_sprite.draw()
         else:
             node_sprite.position = (offset[0] + graph.node[node]["x"] * scale,
-                                         offset[1] + graph.node[node]["y"] * scale)
+                                    offset[1] + graph.node[node]["y"] * scale)
             node_sprite.draw()
